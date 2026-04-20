e18f3925099abc7098782df8cd6580643b217455
Ghoulayush <ramolaayush832@gmail.com>
2026-04-20 22:00:30 +0530

    Doing Changes as per Suggest in phase 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 
-function App() {
+function App(){
   return (
     <div className="app-shell">
       <Navbar />
       <div className="app-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </div>
   );
 }
-
 export default App;
