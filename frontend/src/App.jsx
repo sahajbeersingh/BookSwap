182cc9901be172000b71df6dff528920a242c297
Ghoulayush <ramolaayush832@gmail.com>
2026-04-23 18:31:32 +0530

    Basic changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/Sign";
 import Navbar from "./components/Navbar";
 
 function App(){
